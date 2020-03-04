2d46824227eb86ab3ab58730c362d09f8c917bfe
s-rom <ing.arl.6876@gmail.com>
2020-03-04 16:06:20 +0100

    Added launch files from old repository

--- src/GoalSender.cpp
+++ src/GoalSender.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <nav_msgs/Odometry.h>
+
+#include <iostream>
 #include <string>
 #include <signal.h>
 
@@ -11,6 +14,19 @@ void sigintHandler(int sig)
     ROS_INFO("Process killed by user. Shuting down node.");
     ros::shutdown(); 
 }
+
+void odom_callback(const nav_msgs::Odometry::ConstPtr & msg)
+{
+    auto pos = msg->pose.pose.position;
+    std::cout << "Current pos: ["<<pos.x<<", "<<pos.y<<"]\n";
+}
+
+
+bool compare_points_equals(float x1, float y1, float x2, float y2, float tolerance)
+{
+    return (std::fabs(x1 - x2) <= tolerance && std::fabs(y1 - y2) <=tolerance);
+}
+
 
 
 MoveBaseClient * ac;
@@ -23,6 +39,8 @@ int main(int argc, char **argv)
     MoveBaseClient aux("move_base", true);
     ac = &aux;
 
+    ros::NodeHandle nh("~");
+    ros::Subscriber odom_subscriber = nh.subscribe("odom",1,odom_callback);
 
     while (!ac->waitForServer(ros::Duration(2.0)))
     {
@@ -42,6 +60,7 @@ int main(int argc, char **argv)
         ROS_INFO_STREAM("Params: rosrun package node x y w");
         exit(0);
     }
+
 
 
     move_base_msgs::MoveBaseGoal goal;
