b837439e9c0e9753a7685d65a273c02cf5501ff9
s-rom <ing.arl.6876@gmail.com>
2020-06-08 17:30:26 +0200

    Adds grid lines to canvas

--- web/scripts/simulation.js
+++ web/scripts/simulation.js
@@ -1,8 +1,14 @@
 var cw, ch;
 var server_root = "http://localhost:8080/monitor";
 var positions = null;
-var robot_width = 12;
-var robot_height = 18;
+
+var og_robotWidth = 0.55;
+var og_robotHeight = 0.47;
+
+var robot_width;
+var robot_height;
+
+
 var stageScale = 20;
 var arrow;
 var backColor = '#f5f5f5';
@@ -11,12 +17,14 @@ var deliveryColor = '#24ad37';
 var posRadius = 15;
 var updateRate = 100; //ms
 var simInterval;
+var robotColors = null;
+var colors = ['#404ff5', '#b109d6', '#db0f31', '#faff75', '#e0841b'];
 
 function render(){
     ctx.globalAlpha = 1;
     ctx.fillStyle = backColor;
     ctx.fillRect(0,0,cw,ch);
-
+    
 
     if (positions == null && tasks_info == null){
         ctx.strokeStyle = '#000000';
@@ -26,10 +34,46 @@ function render(){
         ctx.font='16px Courier New';
         ctx.fillText('Waiting for monitor server...', 120, ch/2+40);
         ctx.font = ''
+        return;
     }
 
     renderTasks();
     renderRobots();
+    renderAxis();
+}
+
+function renderAxis()
+{
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.globalAlpha = 1;
+    ctx.strokeStyle = '#000000';
+    ctx.beginPath();
+        ctx.moveTo(0, ch/2);
+        ctx.lineTo(cw, ch/2);
+    ctx.stroke();
+ 
+    ctx.beginPath();
+        ctx.moveTo(cw/2, 0);
+        ctx.lineTo(cw/2, ch);
+    ctx.stroke();
+
+    fontSize = 14;
+    ctx.font=fontSize+'px Courier New';
+    ctx.fillStyle = '#000000';
+    ctx.textAlign = 'center';
+    y = (ch / 2) + (fontSize);
+    ctx.fillText('0,0', cw/2, y);
+    ctx.fillText('5', cw*3/4, y);
+    ctx.fillText('10',cw-fontSize, y);
+    ctx.fillText('-5',cw/4, y);
+    ctx.fillText('-10',fontSize, y);
+
+    x = (cw / 2) + (fontSize);
+    ctx.fillText('5', x, ch/4);
+    ctx.fillText('10',x,fontSize);
+    ctx.fillText('-5',x,ch*3/4);
+    ctx.fillText('-10',x,ch-fontSize);
+    ctx.moveTo(0,0);
 }
 
 function stageToCanvasPoint(point){
@@ -104,24 +148,17 @@ function renderRobots(){
 
 
         ctx.translate(cx, cy);
-        ctx.rotate((-90-pos.yaw) * Math.PI / 180.0);
+        ctx.rotate((-pos.yaw) * Math.PI / 180.0);
         ctx.translate(-cx, -cy);
 
-        ctx.fillStyle = '#d4112b';
+
+        ctx.fillStyle = colors[i % colors.length];
         ctx.fillRect(x, y, robot_width, robot_height);
         ctx.fillStyle = '#000000';
         ctx.strokeRect(x, y, robot_width, robot_height);
-        ctx.fillStyle = '#3262a8';
 
-        
-        // ctx.beginPath();
-        // ctx.arc(cx, cy, 20, 0, 2 * Math.PI);
-        // ctx.stroke();
 
-        ctx.translate(cx, cy);
-        ctx.rotate(Math.PI/2);
-        ctx.translate(-cx, -cy);
-        ctx.drawImage(arrow, x, y, 30, 20);
+        ctx.drawImage(arrow, x+7, cy-9, 30, 20);
 
     }
 
@@ -159,8 +196,7 @@ var requestAnimationFrame = window.requestAnimationFrame;
 var lastUpdate = new Date().getTime();
 
 function tick() {
-    cw = $('#main_canvas').width();
-    ch = $('#main_canvas').height();
+   
     var currentTime = new Date().getTime();
     var deltaTime = currentTime - lastUpdate;
     update(currentTime - lastUpdate);
@@ -174,6 +210,11 @@ function tick() {
 function startSimulation() {
     canvas = document.getElementById('main_canvas');
     ctx = canvas.getContext('2d');
-    // todo: initialize state
+    cw = $('#main_canvas').width();
+    ch = $('#main_canvas').height();
+    let scaleChange = cw / stageScale; 
+    robot_width = og_robotWidth * scaleChange;
+    robot_height = og_robotHeight * scaleChange;
+
     requestAnimationFrame(tick);
 }
